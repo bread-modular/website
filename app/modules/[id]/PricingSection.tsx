dc0c419676b24aa444c84c880b7b3d05d8f16e0a
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-02-22 16:03:13 +0530

    Change the text of what's the difference?

--- app/modules/[id]/PricingSection.tsx
+++ app/modules/[id]/PricingSection.tsx
@@ -31,7 +31,7 @@ export default function PricingSection({ versions }: Props) {
             </option>
           ))}
         </select>
-        <a href="#" className={styles.differenceLink}>{"What's the difference?"}</a>
+        <a href="#" className={styles.differenceLink}>{"difference?"}</a>
       </div>
       <div className={styles.buyOptions}>
         <div className={styles.price}>${selectedVersion.price}</div>
